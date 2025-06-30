72bfd4262b03a66c2c1ed3921776e1375cd06e31
Nagendrakumar19092004 <thumma00001@gmail.com>
2025-06-30 11:24:58 +0530

    update header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -11,18 +11,18 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
   return (
-    <header className="bg-white border-b border-gray-200 px-6 py-4">
+    <header className="bg-white border-b border-gray-200 px-6 py-3 ml-6 mr-10 mt-2 rounded-sm shadow-md">
       <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-semibold text-gray-900 pl-5">{title}</h1>
+        <h1 className="text-3xl font text-gray-900 pl-5">{title}</h1>
 
-        <div className="flex items-center space-x-4 px-8">
-          <div className="relative">
+        <div className="flex items-center space-x-4 px-8 ">
+          <div className="relative mx-10">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
             <Input placeholder="Quick Search..." className="pl-10 w-80" />
           </div>
-           <div className="text-4xl font-thin pl-8">|</div>
+           {/* <div className="text-4xl font-thin pl-8">|</div> */}
           <Button variant="ghost" size="icon">
-            <Bell className="w-5 h-5" />
+            <Bell className="w-5 h-5 " />
           </Button>
 
           <div className="flex items-center space-x-3 px-8">
